152d3c74ad9abfd7c5e6e2d53be62174ce642d14
Matteo Fenu <matfen97@gmail.com>
2020-02-26 11:12:18 +0100

    Added a getSubscriber Middleware to subscribers.js

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -13,12 +13,22 @@ router.get('/', async (req, res) => {
 })
 
 // Getting one
-router.get('/:id', (req, res) => {
-    res.send(req.params.id)
+router.get('/:id', async (req, res, next) => {
+
 })
 
 // Creating one
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+    const subscriber = new Subscriber({
+        name: req.body.name,
+        subscribedToChannel: req.body.subscribedToChannel
+    })
+    try {
+        const newSubscriber = await subscriber.save()
+        res.status(201).json(newSubscriber)
+    } catch (error) {
+        res.status(400).json({ message: err.message })
+    }
 })
 
 // Updating one
@@ -28,5 +38,21 @@ router.put('/:id', (req, res) => {
 // Deleting one
 router.delete('/:id', (req, res) => {
 })
+
+
+// Middleware get subscribers
+async function getSubscriber(req, res, next) {
+    let subscriber
+    try {
+        subscriber = await Subscriber.findById(req.params.id)
+        if (subscriber == null) {
+            return res.status(404).json({ message: 'Cannot find subscriber' })
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message })
+    }
+    res.subscriber = subscriber
+    next()
+}
 
 module.exports = router
